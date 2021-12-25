ed591173f88fe55c10f051b2c8d0cab810a4270d
Mutcholoko <46263572+Mutcholoko@users.noreply.github.com>
2021-12-25 14:53:02 -0300

    now it draws a really basic sphere

--- MyHappyRaytracer/Main.c
+++ MyHappyRaytracer/Main.c
@@ -1,19 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include "Color.h"
 #include "Ray.h"
 #include "Vector3.h"
 
-color ray_color(Ray r) {
+//returns if the ray hits sphere
+bool hit_Sphere(point3 Center, double radius, Ray r) {
+	Vector3 oc = operator_difOfTwoVect3(r.origin, Center);
+	double a = dot(r.direction, r.direction);
+	double b = 2.0 * dot(oc, r.direction);
+	double c = dot(oc, oc) - (radius * radius);
+	double discriminant = (b * b) - (4 * a * c);
+	
+	return(discriminant > 0);
+}
+
+//return color red for ray that hits sphere
+color ray_Color(Ray r) {
+	point3 sphere_c = fill_Vector(0, 0, -1);
+	if (hit_Sphere(sphere_c, 0.5, r)) {
+		color color_return = fill_Vector(1, 0, 0); //color red
+		return color_return;
+	}
 	Vector3 unit_dir = unit_vec(r.direction);
 	double t = 0.5 * (y(unit_dir) + 1.0);
 	color aux_color1, aux_color2;
 	aux_color1 = fill_Vector(1.0, 1.0, 1.0);
 	aux_color2 = fill_Vector(0.5, 0.7, 1.0);
-	color color_return = operator_sumOfTwoVect3(operator_multiplyVect3ByT(aux_color1, (1.0-t)), operator_multiplyVect3ByT(aux_color2, t));
-	return color_return;
+	color color_return = operator_sumOfTwoVect3(operator_multiplyVect3ByT(aux_color1, (1.0 - t)), operator_multiplyVect3ByT(aux_color2, t));
+	return color_return; //if is not the ball, then return the bg color
 }
-
 
 int main(void) {
 
@@ -55,7 +72,7 @@ int main(void) {
 			Vector3 rayDir = operator_difOfTwoVect3(operator_sumOfTwoVect3(operator_sumOfTwoVect3(lower_left_corner, operator_multiplyVect3ByT(horizontal, u)), operator_multiplyVect3ByT(vertical, v)), origin);
 			ray = fill_RayDirection(ray, rayDir);
 
-			color p_color = ray_color(ray);
+			color p_color = ray_Color(ray);
 			write_color(p_color);
 		}
 	}
